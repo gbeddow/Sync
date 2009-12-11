d28174c4dd6afa5af3cc9104f95e2bf6b87f9e5d
gbeddow <greg@gbeddow.com>
2009-12-11 13:02:17 -0800

    require use of SSL in all, and only, places that need it like Sign Up and Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,24 @@ class ApplicationController < ActionController::Base
   # gbeddow - http://usefulfor.com/ruby/2008/06/06/restful_authentication-howto-step-by-step-part-2/
   include AuthenticatedSystem
 
+  # gbeddow - http://www.buildingwebapps.com/articles/6401-using-ssl-in-rails-applications
+  include SslRequirement
+
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+  def ssl_required?
+    # (comment this line out if you want to test ssl locally)
+    return false if local_request? 
+
+    # (uncomment this line if you want to disable ssl in test mode)
+    # return false if RAILS_ENV == 'test'
+
+    # otherwise, use the filters
+    super
+  end
+
 end
